3647e90061dd407f8e0e459b2a3175f993b84bba
Aaron <Aaron.joseph@outlook.com>
2018-12-29 11:12:45 -0700

    Initial commit

---
+++ app/src/main/java/org/poptarticus/punsAd/Pages/Puns.java
@@ -0,0 +1,83 @@
+package org.poptarticus.punsAd.Pages;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import org.poptarticus.punsAd.Books.punBook;
+import org.poptarticus.punsAd.ColorBooks.punsColorWheel;
+import org.poptarticus.punsAd.R;
+
+import java.util.Random;
+
+public class Puns extends Activity {
+
+
+    public static final String TAG = Puns.class.getSimpleName();
+
+    private AdView mAdView;
+
+    private punBook mPunBook = new punBook();
+    private punsColorWheel mPunsColorWheel = new punsColorWheel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puns);
+
+        final TextView PunLabel = (TextView) findViewById(R.id.punTextView);
+        final Button showPunButton = (Button) findViewById(R.id.showPunButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.PunsScreen);
+
+
+        // This sets the text view when you open the puns activity
+        int randomIndexOpen = new Random().nextInt(mPunBook.mPuns.length);
+        final String randomString = mPunBook.mPuns[randomIndexOpen];
+
+        PunLabel.setText(randomString);
+
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                // This sets the text view when the button is clicked
+                int randomIndex = new Random().nextInt(mPunBook.mPuns.length);
+                String randomString = mPunBook.mPuns[randomIndex];
+
+                PunLabel.setText(randomString);
+
+
+                int color = mPunsColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showPunButton.setTextColor(color);
+
+
+            }
+
+        };
+        showPunButton.setOnClickListener(listener);
+    }
+
+    public void imageButton1(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+}
