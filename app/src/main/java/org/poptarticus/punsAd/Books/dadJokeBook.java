bf05dacb88c6b448e54885a035e7d6f176203204
Aaron <Aaron.joseph@outlook.com>
2018-12-29 11:24:11 -0700

    Adding more edits

--- app/src/main/java/org/poptarticus/punsAd/Books/dadJokeBook.java
+++
@@ -1,8 +0,0 @@
-package org.poptarticus.punsAd.Books;
-
-public class dadJokeBook {
-
-    public String[] mDadJoke = {
-            "My wife is really mad at the fact that I have no sense of direction. So I packed up my stuff and right."
-    };
-}
